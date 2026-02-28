3bf3793521772dccb740fee4d7f7e79d016ad898
gitcrazygitwild <hr.schwartz23@gmail.com>
2026-02-27 19:19:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -54,14 +54,27 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
 // Paste your Firebase config here:
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "...",
-  // authDomain: "...",
-  // projectId: "...",
-  // storageBucket: "...",
-  // messagingSenderId: "...",
-  // appId: "..."
+  apiKey: "AIzaSyBEXNyX6vIbHwGCpI3fpVUb5llubOjt9qQ",
+  authDomain: "huberts-house.firebaseapp.com",
+  projectId: "huberts-house",
+  storageBucket: "huberts-house.firebasestorage.app",
+  messagingSenderId: "233498547172",
+  appId: "1:233498547172:web:e250d2f14b0e19c6322df1",
+  measurementId: "G-CX5MN6WBFP"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 let db, eventsCol;
 
